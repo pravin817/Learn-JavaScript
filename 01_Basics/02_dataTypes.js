219975fb98121a8b6eb76a84be92cf3b79abddc9
pravin817 <83346334+pravin817@users.noreply.github.com>
2023-12-18 02:37:48 +0000

    Datatypes added sucessfully.

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(2+4) // we are using nodeJS, not browser
+
+console.log(3 + 3); // Avoid this type because the code readablity should be high
+
+console.log("Pravin Mhaske");
+
+// Data types
+
+var name = "Pravin";
+var age = 22;
+var isLoggedIn = true;
+
+// Number  => 2 to power 53
+// bigInt
+// String => ""
+// Boolean => true/false
+// null => standalone value
+// undefined =>
+// Symbol => Unique
+
+// Object and Array
+
+// Typeof
+
+console.log(typeof undefined);   // undefined
+console.log(typeof null);        // object
+
+/*
+    Note : The typeof null is ---> Object,
+                      undefined ---> undefined
+*/
