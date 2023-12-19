783928b242695de2877c3099fbd608dce005c1d3
pravin817 <83346334+pravin817@users.noreply.github.com>
2023-12-19 01:14:33 +0000

    Comparison added successfully.

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" == 2);
+// console.log("2" === 2);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null != 0);
+// console.log(null >= 0);
+
+/*
+    Note : Avoid the above types of the conversion.
+           Always give priority to clean code.
+*/
+
+// console.log(undefined > 0);   // return the false in all cases.
+// console.log(undefined < 0);
+// console.log(undefined == 0);
