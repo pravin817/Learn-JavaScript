58b2b3649f17b509b0c7bd86f5974230ca11e918
pravin817 <83346334+pravin817@users.noreply.github.com>
2023-12-20 13:04:58 +0000

    Function added successfully.

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -24,9 +24,9 @@ const result = addTwoNumbers(5, 2);
 // console.log("Result : ",result)
 
 function loginUserMessage(username = "user") {
-//   if (!username) {
-//     // dosomething
-//   }
+  //   if (!username) {
+  //     // dosomething
+  //   }
 
   if (username === undefined) {
     console.log("Please enter username.");
@@ -35,5 +35,35 @@ function loginUserMessage(username = "user") {
   return `${username} just logged In.`;
 }
 
-console.log(loginUserMessage("Pravin"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage("Pravin"));
+// console.log(loginUserMessage());
+
+// Rest operator
+function calculateCartPrice(val1, val2, ...num1) {
+  return num1;
+}
+
+// console.log(calculateCartPrice(10, 20, 30, 40));
+
+const user = {
+  name: "Pravin",
+  price: 999,
+};
+
+function handleObject(anyObject) {
+  console.log(`username is : ${anyObject.name} and price : ${anyObject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+  name: "Ganesh",
+  price: 10000,
+});
+
+const arr = [1, 2, 3, 4, 6, 9];
+
+function returnSecondValue(getArr) {
+  return getArr[1];
+}
+
+// console.log(returnSecondValue(arr));
