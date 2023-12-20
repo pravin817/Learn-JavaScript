ec83f933aac8a7944511da31658a1887021d572e
pravin817 <83346334+pravin817@users.noreply.github.com>
2023-12-20 13:11:06 +0000

    Scope added successfully.

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,20 @@
+let a = 300
+
+if (true) {
+  let a = 10;
+  console.log(a);
+  const b = 20;
+  var c = 30;
+}
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+
+
