38ef1efcbac2869311b51b30096aa97f25c94d0b
pravin817 <83346334+pravin817@users.noreply.github.com>
2023-12-20 07:14:56 +0000

    Math and Number added successfully.

---
+++ 01_Basics/06_numbers_and_math.js
@@ -0,0 +1,39 @@
+// const score = 400;
+
+// const balance = new Number(100);
+
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 123.8567;
+
+// console.log(otherNumber.toPrecision(3));
+
+
+// const hundreds = 123456789;
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// ++++++++++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++
+
+console.log(Math)
+// console.log(Math.abs(2.3))
+// console.log(Math.abs(-2.3))
+
+
+// console.log(Math.round(4.5));
+
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(2,3,4,56));
+// console.log(Math.max(1,2,3,4));
+
+// console.log(Math.random())
+
+const min = 10;
+const max = 20;
+
+const randomNumber = Math.floor((Math.random()*(max-min+1)) + min);
+console.log(randomNumber)
