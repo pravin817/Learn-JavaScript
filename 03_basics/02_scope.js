e8621f3d948f68ec693b89ef0b4464dcceb59148
pravin817 <83346334+pravin817@users.noreply.github.com>
2023-12-20 13:35:02 +0000

    Scope Added successfully.

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,20 +1,57 @@
-let a = 300
+let a = 300;
 
 if (true) {
   let a = 10;
-  console.log(a);
+  //   console.log(a);
   const b = 20;
   var c = 30;
 }
 
-
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    
+// for (let i = 0; i < array.length; i++) {
+//   const element = array[i];
+// }
+
+function one() {
+  const username = "Pravin";
+
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+
+  //   console.log(website);
+
+  //   two();
 }
 
+// one();
 
+// if (true) {
+//   const username = "Pravin";
+
+//   if (username === "Pravin") {
+//     const website = " youtube";
+//     console.log(username + website);
+//   }
+
+//   console.log(website);
+// }
+
+// console.log(username);
+
+// +++++++++++++++++++++++++++++++++++++++ Interesting +++++++++++++++++++++
+console.log(addOne(2));
+
+function addOne(num) {
+  return num + 1;
+}
+
+console.log(addTwo(3));
+
+const addTwo = function (num) {
+  return num + 2;
+};
