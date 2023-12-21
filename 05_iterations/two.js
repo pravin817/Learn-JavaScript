ab8f3f0d43a942ea019a376911f62b8d3072c59b
pravin817 <83346334+pravin817@users.noreply.github.com>
2023-12-21 02:12:11 +0000

    While, do-while loop added successfully.

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+// let i = 0;
+// while (i <= 10) {
+//   console.log(`Value of index is : ${i}`);
+//   i = i+2;
+// }
+
+// const myArr = ["Flash", "Batman", "Superman"];
+
+// let arr = 0;
+
+// while(arr<myArr.length){
+//     console.log(`Value is : ${myArr[arr]}`);
+//     arr++;
+// }
+
+let score = 11;
+do {
+  console.log(`Score is : ${score}`);
+  score++;
+} while (score <= 10);
